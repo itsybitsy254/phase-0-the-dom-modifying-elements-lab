ac14489d095f247754a6c0b2be35c96ecc3725eb
Vitalis Ian <viterlis254@gmail.com>
2024-06-23 11:33:44 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainToRemove = document.querySelector('main#main');
+if (mainToRemove) {
+  mainToRemove.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Vitalis is the champion";
+
+document.body.appendChild(newHeader);
